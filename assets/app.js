d3e4c24004969bf0e32d84bd7f86ab949663f887
AKTUAL MEDIA <kamera1337@gmail.com>
2026-05-27 15:34:21 +0200

    Osvježi cache ključ za kompaktni BPP i profinjeni ASG prikaz

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var VERSION = '20260527-bpp-share-asg01';
+  var VERSION = '20260527-bpp-compact-asg02';
 
   var nativeFetch = window.fetch && window.fetch.bind(window);
   if (nativeFetch && !window.__gnkRootDataFetch) {
